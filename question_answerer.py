e80c47453d3a069b3ab6488d2cffca840472cfe7
Oliver Morland <oliversimonedward@hotmail.co.uk>
2025-05-09 10:24:30 -0400

    added examples to question answerer

--- question_answerer.py
+++ question_answerer.py
@@ -26,13 +26,18 @@ class QuestionAnswerer:
 
 if __name__ == "__main__":
     question_answerer = QuestionAnswerer(model_path="psr_qa_fine_tuned_model")
-    address_examples = [
+    examples = [
         "ct. 1: commercial  burglary  ct. 2: theft of  property/  pulaski county circuit  court, little rock, ar;  docket no.: cr 19- 4529  t was represented by counse urglarizing the office and pro as, on or about september 2 hours and stole items.",
         "illegal re-entry into the  united states after  deportation/  united states district  court, western district  of texas, del rio  division, tx;  docket no.: dr-10- cr-0066-001am  nt was represented by cou ong with six illegal aliens,  iles east of del rio, texa was detained after a short fo ip and he admitted he was a als, did not have any docum s legally. the defendant, al border patrol station for fur eak with agents without the  nt, along with a group of  r, via a boat, near the del r gh the brush and waited fo mputerized records check r ons by border patrol agents",
         "ct. 1: 647(b) pc,  disorderly conduct  (misd.).  los angeles, ca, pd",
         "496(a) pc: receiving  stolen property  (fel.)/ los angeles  superior court;  8",
+        "1)driving while  suspended/revoked  (m),",
         "37-23732b(a)(2):  drug trafficking in  cocaine (cts. 2 - 3)/  canyon county  district court, ut;  docket no.: cr- 2009-20518  represented by counsel.  o court records an addition ed."
     ]
-    for example in address_examples:
+    for example in examples:
         answer = question_answerer.query_context("what is the courthouse address?", example)
+        print(f"Answer: {answer}")
+
+    for example in examples:
+        answer = question_answerer.query_context("What offense was committed?", example)
         print(f"Answer: {answer}")
